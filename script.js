f6bb13d318ebe7f41c9295f5fb2e5b5690537010
Júlio César <114767812+NETUNO14@users.noreply.github.com>
2023-06-08 14:24:31 -0300

    brincando

---
+++ script.js
@@ -0,0 +1,11 @@
+function exibirHoraAtual() {
+    var elementoHora = document.getElementById("hora");
+    var dataHora = new Date();
+    var hora = dataHora.getHours();
+    var minutos = dataHora.getMinutes();
+    var horaFormatada = hora.toString().padStart(2, '0') + ":" + minutos.toString().padStart(2, '0')
+    elementoHora.textContent = horaFormatada;
+}
+
+// Atualiza a hora a cada segundo
+setInterval(exibirHoraAtual, 1000);
